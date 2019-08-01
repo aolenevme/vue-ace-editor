728a9d9ca61bf6f888bd4da18248fdf237e60dd4
Aleksandr Olenev <aleksandr.olenev@leroymerlin.com>
2019-08-01 12:18:29 +0300

    feat/add-ace-editor: Editor is loaded

--- src/AceEditor.vue
+++ src/AceEditor.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="ace-editor">
-    function foo(items) { var x = "All this is syntax highlighted"; return x; }
+    function foo(items) { var x = "All this is syntax highlighteddd"; return x;
+    }
   </div>
 </template>
 
@@ -9,25 +10,13 @@ import ace from "ace-builds/src-noconflict/ace.js";
 export default {
   name: "AceEditor",
   mounted() {
-    // let aceEditorScript = document.createElement("script");
-    // // Set the source of the script
-    // aceEditorScript.setAttribute("src", "ace-builds/src-noconflict/ace.js");
-    //
-    // // Set the type of the script
-    // aceEditorScript.setAttribute("type", "text/javascript");
-    //
-    // // Set the charset of the script
-    // aceEditorScript.setAttribute("charset", "utf-8");
-    //
-    // document.head.appendChild(aceEditorScript);
-  },
-  beforeCreate() {
     var editor = ace.edit("ace-editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/javascript");
   }
 };
 </script>
+
 <style scoped lang="scss">
 #ace-editor {
   position: absolute;
@@ -35,5 +24,8 @@ export default {
   right: 0;
   bottom: 0;
   left: 0;
+
+  width: 100%;
+  height: 100%;
 }
 </style>
