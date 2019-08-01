b7d32b99742b2075c69e82aad2b31616f6665aa0
Alexander Olenev <52192727+eshekak@users.noreply.github.com>
2019-08-01 12:20:01 +0300

    Feat/add ace editor (#2)
    * feat/add-ace-editor: Added ace-editor into package.json
    
    * feat/add-ace-editor: Attempt to run it
    
    * feat/add-ace-editor: Editor is loaded

---
+++ src/AceEditor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="ace-editor">
+    function foo(items) { var x = "All this is syntax highlighteddd"; return x;
+    }
+  </div>
+</template>
+
+<script>
+import ace from "ace-builds/src-noconflict/ace.js";
+export default {
+  name: "AceEditor",
+  mounted() {
+    var editor = ace.edit("ace-editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
+  }
+};
+</script>
+
+<style scoped lang="scss">
+#ace-editor {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+}
+</style>
